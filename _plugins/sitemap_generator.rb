edfe88e8a6283206b2a2597edc8c679872bdb971
Ian D. Eccles <ian.eccles@gmail.com>
2011-06-09 15:10:25 -0400

    breaking sitemaps out into smaller chunks

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -34,6 +34,22 @@ module Jekyll
     alias :to_liquid_without_mtime :to_liquid
     alias :to_liquid :to_liquid_with_mtime
   end
+  
+  class Site
+    def site_payload_with_sitemap
+      sitemaps = pages.select do |p|
+        p.url =~ /sitemap[a-zA-Z0-9\-_]*.xml/
+      end
+      without_sitemap_index = sitemaps.reject { |p| p.url == '/sitemap.xml' }
+      site_payload_without_sitemap.tap do |payload|
+        payload['site']['sitemap_pages'] = pages - sitemaps
+        payload['site']['sitemaps'] = without_sitemap_index
+      end
+    end
+    
+    alias :site_payload_without_sitemap :site_payload
+    alias :site_payload :site_payload_with_sitemap
+  end
 end
 
 Liquid::Template.register_filter(SitemapFilters)
